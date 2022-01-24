a49d3f13778d7cb7d176be9ffbf63f3eb0286bad
Bisio98 <alberto.bisetti@hotmail.it>
2022-01-24 17:00:49 +0100

    snack-4

---
+++ snack-5/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insegnanti e PM</title>
+</head>
+<body>
+    <?php
+        $db = [
+            'teachers' => [
+                [
+                    'name' => 'Michele',
+                    'lastname' => 'Papagni'
+                ],
+                [
+                    'name' => 'Fabio',
+                    'lastname' => 'Forghieri'
+                ]
+            ],
+            'pm' => [
+                [
+                    'name' => 'Roberto',
+                    'lastname' => 'Marazzini'
+                ],
+                [
+                    'name' => 'Federico',
+                    'lastname' => 'Pellegrini'
+                ]
+            ]
+        ];
+    ?>
+
+    <?php for($i = 0; $i < count($db); $i++){ ?> 
+        <?php  ?> 
+    
+</body>
+</html>
